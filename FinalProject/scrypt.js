b44aced800e8007abcf7847d466fac4900d7c0f9
maks-march <98648053+maks-march@users.noreply.github.com>
2022-01-29 17:53:16 +0500

    Add files via upload

---
+++ FinalProject/scrypt.js
@@ -0,0 +1,433 @@
+
+function windows(data)
+{
+
+const quest_1 = document.querySelector('.quest_1');
+const exit_1 = document.querySelector('.exit_1');
+
+
+const quest_2 = document.querySelector('.quest_2');
+const exit_2 = document.querySelector('.exit_2');
+
+
+const quest_3 = document.querySelector('.quest_3');
+const exit_3 = document.querySelector('.exit_3');
+
+
+const quest_4 = document.querySelector('.quest_4');
+const exit_4 = document.querySelector('.exit_4');
+
+
+const quest_5 = document.querySelector('.quest_5');
+const exit_5 = document.querySelector('.exit_5');
+
+
+const quest_6 = document.querySelector('.quest_6');
+const exit_6 = document.querySelector('.exit_6');
+
+
+const quest_7 = document.querySelector('.quest_7');
+const exit_7 = document.querySelector('.exit_7');
+
+
+const quest_8 = document.querySelector('.quest_8');
+const exit_8 = document.querySelector('.exit_8');
+
+if(data == 2) {
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 3) {
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 4) {
+    const btn3 = document.querySelector('.btn3');
+    btn3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    exit_3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 5) {
+    const btn4 = document.querySelector('.btn4');
+    btn4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    exit_4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    const btn3 = document.querySelector('.btn3');
+    btn3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    exit_3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 6) {
+    const btn5 = document.querySelector('.btn5');
+    btn5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    exit_5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    const btn4 = document.querySelector('.btn4');
+    btn4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    exit_4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    const btn3 = document.querySelector('.btn3');
+    btn3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    exit_3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 7) {
+    const btn6 = document.querySelector('.btn6');
+    btn6.addEventListener('click', () => {
+        quest_6.classList.toggle('actived');
+    })
+    exit_6.addEventListener('click', () => {
+        quest_6.classList.toggle('actived');
+    })
+    const btn5 = document.querySelector('.btn5');
+    btn5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    exit_5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    const btn4 = document.querySelector('.btn4');
+    btn4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    exit_4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    const btn3 = document.querySelector('.btn3');
+    btn3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    exit_3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 8) {
+    const btn7 = document.querySelector('.btn7');
+    btn7.addEventListener('click', () => {
+        quest_7.classList.toggle('actived');
+    })
+    exit_7.addEventListener('click', () => {
+        quest_7.classList.toggle('actived');
+    })
+    const btn6 = document.querySelector('.btn6');
+    btn6.addEventListener('click', () => {
+        quest_6.classList.toggle('actived');
+    })
+    exit_6.addEventListener('click', () => {
+        quest_6.classList.toggle('actived');
+    })
+    const btn5 = document.querySelector('.btn5');
+    btn5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    exit_5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    const btn4 = document.querySelector('.btn4');
+    btn4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    exit_4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    const btn3 = document.querySelector('.btn3');
+    btn3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    exit_3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+
+if(data == 27) {
+    const btn8 = document.querySelector('.btn8');
+    btn8.addEventListener('click', () => {
+        quest_8.classList.toggle('actived');
+    })
+    exit_8.addEventListener('click', () => {
+        quest_8.classList.toggle('actived');
+    })
+    const btn7 = document.querySelector('.btn7');
+    btn7.addEventListener('click', () => {
+        quest_7.classList.toggle('actived');
+    })
+    exit_7.addEventListener('click', () => {
+        quest_7.classList.toggle('actived');
+    })
+    const btn6 = document.querySelector('.btn6');
+    btn6.addEventListener('click', () => {
+        quest_6.classList.toggle('actived');
+    })
+    exit_6.addEventListener('click', () => {
+        quest_6.classList.toggle('actived');
+    })
+    const btn5 = document.querySelector('.btn5');
+    btn5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    exit_5.addEventListener('click', () => {
+        quest_5.classList.toggle('actived');
+    })
+    const btn4 = document.querySelector('.btn4');
+    btn4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    exit_4.addEventListener('click', () => {
+        quest_4.classList.toggle('actived');
+    })
+    const btn3 = document.querySelector('.btn3');
+    btn3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    exit_3.addEventListener('click', () => {
+        quest_3.classList.toggle('actived');
+    })
+    const btn2 = document.querySelector('.btn2');
+    btn2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    exit_2.addEventListener('click', () => {
+        quest_2.classList.toggle('actived');
+    })
+    const btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+    exit_1.addEventListener('click', () => {
+        quest_1.classList.toggle('actived');
+    })
+}
+}
+
+{
+setTime();
+setInterval(setTime, 10000000);
+}
+
+function setTime() {
+
+    const time = new Date();
+    const data = time.getDate();
+    const map = document.querySelector('.main');
+    if (data == 2) {
+         
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map1.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        `
+
+    } 
+    if (data == 3) {
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map2.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="4%">`
+    }
+    if (data == 4) {
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map3.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="4%">
+        <img class="btn3 btn" src="gift.png" alt="" width="4%">`
+    }
+    if (data == 5) {
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map4.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="4%">
+        <img class="btn3 btn" src="gift.png" alt="" width="4%">
+        <img class="btn4 btn" src="toy.png" alt="" width="4%">`
+    }
+    if (data == 6) {
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map5.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="4%">
+        <img class="btn3 btn" src="gift.png" alt="" width="4%">
+        <img class="btn4 btn" src="toy.png" alt="" width="%">
+        <img class="btn5 btn" src="tree.png" alt="" width="4%">
+        `
+    }
+    if (data == 7) {
+        
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map6.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="4%">
+        <img class="btn3 btn" src="gift.png" alt="" width="4%">
+        <img class="btn4 btn" src="toy.png" alt="" width="4%">
+        <img class="btn5 btn" src="tree.png" alt="" width="4%">
+        <img class="btn6 btn" src="snow.png" alt="" width="4%">
+        `
+    }
+    if (data == 8) {
+        
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map7.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="4%">
+        <img class="btn3 btn" src="gift.png" alt="" width="4%">
+        <img class="btn4 btn" src="toy.png" alt="" width="4%">
+        <img class="btn5 btn" src="tree.png" alt="" width="4%">
+        <img class="btn6 btn" src="snow.png" alt="" width="4%">
+        <img class="btn7 btn" src="cookie.png" alt="" width="4%">
+        `
+        
+    }
+    if (data == 27) {
+        map.innerHTML = 
+        `
+        <h1>Happy New Year</h1>
+        <img class="dec1" src="png-transparent-pushpin-push-pin-office-pin-red-tack-thumbtack-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="dec2" src="png-clipart-green-thumbtack-illustration-drawing-pin-bulletin-board-pin-pin-data-removebg-preview.png" alt="" srcset="" width="100px">
+        <img class="map" src="map8.png" alt="" srcset="" width="85%">
+        <img class="btn1 btn" src="642012.png" alt="" width="4%">
+        <img class="btn2 btn" src="sock.png" alt="" width="6%">
+        <img class="btn3 btn" src="gift.png" alt="" width="4%">
+        <img class="btn4 btn" src="toy.png" alt="" width="4%">
+        <img class="btn5 btn" src="tree.png" alt="" width="4%">
+        <img class="btn6 btn" src="snow.png" alt="" width="4%">
+        <img class="btn7 btn" src="cookie.png" alt="" width="4%">
+        <img class="btn8 btn" src="bells.png" alt="" width="4%">`
+    }
+    windows(data);
+}
